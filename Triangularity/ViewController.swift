08db554837f1b8bb9b8f2d4a23de706ed9ac2da2
Sitdhibong Laokok <sitdhibong@gmail.com>
2015-10-23 18:09:32 +0700

    Initial Commit

---
+++ Triangularity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Triangularity
+//
+//  Created by Sitdhibong Laokok on 23/10/58.
+//  Copyright © พ.ศ. 2558 We Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
